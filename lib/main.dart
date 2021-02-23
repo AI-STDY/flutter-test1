c74d59f69f3e5de2ffede9ad0c56e99183400227
kitaoka <pt.ai-kitaoka@tgl.co.jp>
2021-02-23 20:18:10 +0900

    タイトル変更

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
           appBar: AppBar(
-            title: const Text('youtubeアプリdayo'),
+            title: const Text('youtubeアプリ'),
             leading: Icon(Icons.videocam),
             actions: <Widget>[
               SizedBox(
